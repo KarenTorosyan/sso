80cc1f297bf99212b1d6ffee121444e920b40749
KarenTorosyan <karen.torosyan@outlook.com>
2023-09-30 17:48:58 +0400

    testcontainers mongo optimization

--- sso/src/test/java/auth/IntegrateTestContainers.java
+++ sso/src/test/java/auth/IntegrateTestContainers.java
@@ -1,6 +1,8 @@
 package auth;
 
 import auth.configs.db.SupportedDatabases;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
@@ -8,12 +10,24 @@ import org.testcontainers.containers.MongoDBContainer;
 public interface IntegrateTestContainers {
 
     class Mongo {
+
+        public static final MongoDBContainer container =
+                new MongoDBContainer("mongo:7.0.1-jammy");
+
+        @BeforeAll
+        static void startContainer() {
+            container.start();
+        }
+
         @DynamicPropertySource
         static void propertiesRegistry(DynamicPropertyRegistry propertyRegistry) {
-            MongoDBContainer container = new MongoDBContainer("mongo:7.0-rc-jammy");
-            container.start();
             propertyRegistry.add("spring.data.mongodb.uri", container::getReplicaSetUrl);
             propertyRegistry.add("db.selected", SupportedDatabases.MONGO::name);
+        }
+
+        @AfterAll
+        static void stopContainer() {
+            container.stop();
         }
     }
 }
